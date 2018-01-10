54b406b5685ef5b89abe13fa36976ea34f93b8bf
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-01-10 15:06:48 -0200

    cnfigurando index.hmtl

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const sass = require('gulp-sass')
 const livereload = require('gulp-livereload')
 
 const pathToFiles = ['./assets/sass/*.sass', './assets/sass/**/*.sass']
+const pathToHtml = ['./*.html']
 
 gulp.task('sass', () => {
     return gulp.src(pathToFiles)
@@ -12,8 +13,19 @@ gulp.task('sass', () => {
         .pipe(gulp.dest('./public/css'))
         .pipe(livereload({ start: true }))
 })
- 
+
+gulp.task('html', function () {
+    return gulp.src(pathToHtml)
+        .pipe(gulp.dest(''))
+        .pipe(livereload({ start: true} ))
+})
+
 gulp.task('sass:watch', () => {
     livereload.listen()
     gulp.watch(pathToFiles, ['sass'])
+})
+
+gulp.task('watch', () => {
+    livereload.listen()
+    gulp.watch([pathToFiles, pathToHtml], ['sass','html'])
 })
