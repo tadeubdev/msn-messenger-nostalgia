6b0560f2253c31888cc5339bb7dc2daacb1dffc0
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-02-24 12:19:12 -0300

    Correct autoprefixer name

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 'use strict'
 
 let gulp = require("gulp")
-let autoprefixer = require("gulp-autoprefixer")
+let prefix = require("gulp-autoprefixer")
 let cleancss = require("gulp-clean-css")
 let concat = require("gulp-concat")
 let livereload = require("gulp-livereload")
 let rename = require("gulp-rename")
 let sass = require("gulp-sass")
+
+gulp.task('sass', function() {
+    gulp.src('assets/sass/*.sass')
+    .pipe(sass())
+    .pipe(prefix({
+        browsers: ['last 5 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('public/css/'))
+})
