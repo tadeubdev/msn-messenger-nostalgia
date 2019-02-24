600facf4d09e24467069d70ea39ffaedd41f0ef7
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-02-24 13:45:06 -0300

    Removes css/* from .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
 'use strict'
 
-let gulp = require("gulp")
-let prefix = require("gulp-autoprefixer")
-let cleancss = require("gulp-clean-css")
-let concat = require("gulp-concat")
-let rename = require("gulp-rename")
-let sass = require("gulp-sass")
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const livereload = require('gulp-livereload')
 
-gulp.task('sass', function() {
-    gulp.src('assets/sass/*.sass')
-    .pipe(sass())
-    .pipe(prefix({
-        browsers: ['last 5 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('public/css/'))
+const pathToFiles = ['./assets/sass/*.sass', './assets/sass/**/*.sass']
+const pathToHtml = ['./*.html']
+
+gulp.task('sass', () => {
+    return gulp.src(pathToFiles)
+        .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'))
+        .pipe(livereload({ start: true }))
+})
+
+gulp.task('html', function () {
+    return gulp.src(pathToHtml)
+        .pipe(gulp.dest(''))
+        .pipe(livereload({ start: true} ))
+})
+
+gulp.task('sass:watch', () => {
+    livereload.listen()
+    gulp.watch(pathToFiles, ['sass'])
+})
+
+gulp.task('watch', () => {
+    livereload.listen()
+    gulp.watch([pathToFiles, pathToHtml], ['sass','html'])
 })
