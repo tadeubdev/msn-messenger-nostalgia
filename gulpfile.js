fa7d563e76857c4233a0c725a03b09f1ef41f052
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-02-24 13:45:57 -0300

    Remove unnecessaries libraries from gulp - That commit remove all unnecessaries libraries includding: livereload etc

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,11 @@
 
 const gulp = require('gulp')
 const sass = require('gulp-sass')
-const livereload = require('gulp-livereload')
 
-const pathToFiles = ['./assets/sass/*.sass', './assets/sass/**/*.sass']
-const pathToHtml = ['./*.html']
+const pathToFiles = ['./assets/sass/**/*.sass']
 
 gulp.task('sass', () => {
     return gulp.src(pathToFiles)
         .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
         .pipe(gulp.dest('./public/css'))
-        .pipe(livereload({ start: true }))
-})
-
-gulp.task('html', function () {
-    return gulp.src(pathToHtml)
-        .pipe(gulp.dest(''))
-        .pipe(livereload({ start: true} ))
-})
-
-gulp.task('sass:watch', () => {
-    livereload.listen()
-    gulp.watch(pathToFiles, ['sass'])
-})
-
-gulp.task('watch', () => {
-    livereload.listen()
-    gulp.watch([pathToFiles, pathToHtml], ['sass','html'])
 })
