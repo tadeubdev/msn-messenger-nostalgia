6c43601d1411f896abcc4b55057d21926990c0bf
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-11-20 10:24:55 -0300

    corrige dist path

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'msn-messenger-nostalgia/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
